fef2e683c2c9a5a198cf2022a3bdead64c83dd9d
MayankDembla <43243575+MayankDembla@users.noreply.github.com>
2020-05-17 06:54:40 +0530

    First Commit for getting answer of 'why typescript'

---
+++ app.ts
@@ -0,0 +1,24 @@
+console.log("Your Code here .... ")
+
+
+
+function add(n1,n2){
+    return n1+n2 ;
+}
+
+
+
+const number1 = '5' ; 
+const number2 = 2.8 ; 
+
+const result = add(number1,number2) ;
+
+console.log(result) ; 
+
+// Now this is the case where JavaScript has a Demerit 
+// output for this is 52.8 
+// this is not at all is correct 
+
+
+// Now the Type Script comes into picture.
+
