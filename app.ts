eaeca6341b5a9904d3416b6ef3e7a438766189dd
MayankDembla <43243575+MayankDembla@users.noreply.github.com>
2020-05-17 06:59:11 +0530

    Using TypeScript to solve the problem

--- app.ts
+++ app.ts
@@ -2,13 +2,13 @@ console.log("Your Code here .... ")
 
 
 
-function add(n1,n2){
+function add(n1: number,n2: number){
     return n1+n2 ;
 }
 
-
-
-const number1 = '5' ; 
+// Making a Strict typing ... 
+// We Compile/ transpile the code to js at last.
+const number1 = 5 ; 
 const number2 = 2.8 ; 
 
 const result = add(number1,number2) ;
